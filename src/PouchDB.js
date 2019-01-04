d5f52b008700c9b7b680907363f926ca6665d216
Arno Saine <arno@mowhi.com>
2019-01-05 01:00:12 +0200

    Reconnect if PouchDB props change

--- src/PouchDB.js
+++ src/PouchDB.js
@@ -1,32 +1,6 @@
-import { Component } from 'react';
-import { node, number, string } from 'prop-types';
-import { create, close } from './pouchdbConnections';
 import { Provider } from './DBContext';
+import { useDBOptions } from './useDB';
 
-export default class PouchDB extends Component {
-  static propTypes = {
-    children: node,
-    maxListeners: number,
-    name: string.isRequired
-  };
-
-  static defaultProps = {
-    children: null,
-    maxListeners: undefined
-  };
-
-  constructor(props) {
-    super(props);
-    const { children, ...options } = props;
-    this.db = create(options);
-  }
-
-  componentWillUnmount() {
-    const { children, ...options } = this.props;
-    close(options);
-  }
-
-  render() {
-    return <Provider value={this.db}>{this.props.children}</Provider>;
-  }
+export default function PouchDB({ children, ...options }) {
+  return <Provider value={useDBOptions(options)}>{children}</Provider>;
 }
--- src/useDB.js
+++ src/useDB.js
@@ -1,28 +1,50 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import PouchDB from 'pouchdb';
+import stringify from 'fast-json-stable-stringify';
 import DBContext from './DBContext';
-import { create } from './pouchdbConnections';
+import { create, close } from './pouchdbConnections';
+
+export function useDBOptions(options) {
+  const key = stringify(options);
+  const [prevOptions, setPrevOptions] = useState(options);
+  const [prevKey, setPrevKey] = useState(key);
+  const [db, setDB] = useState(options ? () => create(options) : undefined);
+
+  if (key !== prevKey) {
+    if (prevOptions) {
+      close(prevOptions);
+    }
+    if (options) {
+      setDB(create(options));
+    }
+    setPrevOptions(options);
+    setPrevKey(key);
+  }
+
+  return db;
+}
+
+export function useDBContext() {
+  return useContext(DBContext);
+}
 
 export default function useDB(
-  db,
+  any,
   { callee = 'useDB', example = 'useDB(options)' } = {}
 ) {
-  if (db) {
-    if (db instanceof PouchDB) {
-      return db;
-    }
-    return create(db);
+  const dbInstance = any instanceof PouchDB ? any : undefined;
+  const options = dbInstance ? undefined : any;
+  const dbOptions = useDBOptions(options);
+  const dbContext = useDBContext();
+
+  const db = dbInstance || dbOptions || dbContext;
+
+  if (!db) {
+    throw new Error(
+      callee
+        ? `\`${callee}\` was called without \`db\` and database is not in context. Provide database using <PouchDB /> or \`${example}\``
+        : 'Database is not in context. Provide database using <PouchDB />'
+    );
   }
-
-  const dbContext = useContext(DBContext);
-
-  if (dbContext) {
-    return dbContext;
-  }
-
-  throw new Error(
-    callee
-      ? `\`${callee}\` was called without \`db\` and database is not in context. Provide database using <PouchDB /> or \`${example}\``
-      : 'Database is not in context. Provide database using <PouchDB />'
-  );
+  return db;
 }
