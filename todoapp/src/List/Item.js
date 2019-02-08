3b3cbd118b50b9a02b80bcd6abb6dab53c0fba8d
Arno Saine <arno@mowhi.com>
2019-02-08 12:34:17 +0200

    Save changes on enter

--- todoapp/src/List/Item.js
+++ todoapp/src/List/Item.js
@@ -2,18 +2,26 @@ import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import { useDB } from 'react-pouchdb/browser';
 
-export default function Item({ doc, doc: { completed = false } }) {
+export default function Item({ doc, doc: { completed = false, title } }) {
   const { put, remove } = useDB();
   const [focus, setFocus] = useState();
   const [isEditing, setIsEditing] = useState();
-  const [title, setTitle] = useState(doc.title);
   const inputRef = useRef();
+
   useEffect(() => {
     if (focus) {
       inputRef.current.focus();
       setFocus(false);
     }
   });
+
+  function handleSave() {
+    setIsEditing(false);
+    put({
+      ...doc,
+      title: inputRef.current.value.trim()
+    });
+  }
 
   return (
     <li
@@ -42,15 +50,13 @@ export default function Item({ doc, doc: { completed = false } }) {
         className="edit"
         ref={inputRef}
         type="text"
-        value={title}
-        onChange={({ target: { value } }) => setTitle(value)}
-        onBlur={() => {
-          setIsEditing(false);
-          put({
-            ...doc,
-            title
-          });
+        defaultValue={title}
+        onKeyDown={({ keyCode }) => {
+          if (keyCode === 13) {
+            handleSave();
+          }
         }}
+        onBlur={handleSave}
       />
     </li>
   );
