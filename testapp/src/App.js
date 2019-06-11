edc616ee601876e2421a169498106637aea96e7a
Arno Saine <arno@mowhi.com>
2019-06-11 20:56:32 +0300

    Find document

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,4 +1,4 @@
-import { PouchDB, Get } from 'react-pouchdb/browser';
+import { PouchDB, Get, Find } from 'react-pouchdb/browser';
 import ErrorBoundaryAndSuspenseOrder from './ErrorBoundaryAndSuspenseOrder';
 import InitializeDB from './InitializeDB';
 
@@ -8,8 +8,20 @@ function App() {
       <h1>Test React PouchDB</h1>
       <InitializeDB>
         <ErrorBoundaryAndSuspenseOrder>
+          <h3>Get document</h3>
           <PouchDB name="test">
             <Get id="a" render={({ doc }) => (doc ? doc.value : 'not found')} />
+          </PouchDB>
+        </ErrorBoundaryAndSuspenseOrder>
+        <ErrorBoundaryAndSuspenseOrder>
+          <h3>Find document</h3>
+          <PouchDB name="test">
+            <Find
+              selector={{
+                _id: 'a'
+              }}
+              render={({ docs }) => docs?.[0]?.value ?? 'not found'}
+            />
           </PouchDB>
         </ErrorBoundaryAndSuspenseOrder>
       </InitializeDB>
