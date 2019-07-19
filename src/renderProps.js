d856b9ee74f92585d67963b9033e530cd77e4bf8
Arno Saine <arno@mowhi.com>
2019-07-19 17:21:39 +0300

    Name function `useRenderProps`

--- src/renderProps.js
+++ src/renderProps.js
@@ -1,42 +1,37 @@
 import { Children, cloneElement, createElement } from 'react';
 import useDB from './useDB';
 
-export default (useDBOperation, error) => ({
-  db,
-  children,
-  component,
-  render,
-  ...otherProps
-}) => {
-  const props = {
-    db: useDB(db, error)
-  };
-  try {
-    Object.assign(
-      props,
-      db ? useDBOperation(db, otherProps) : useDBOperation(otherProps)
-    );
-  } catch (error) {
-    if (component || render) {
-      throw error;
+export default (useDBOperation, error) =>
+  function useRenderProps({ db, children, component, render, ...otherProps }) {
+    const props = {
+      db: useDB(db, error)
+    };
+    try {
+      Object.assign(
+        props,
+        db ? useDBOperation(db, otherProps) : useDBOperation(otherProps)
+      );
+    } catch (error) {
+      if (component || render) {
+        throw error;
+      }
     }
-  }
 
-  if (component) {
-    return createElement(component, props);
-  }
+    if (component) {
+      return createElement(component, props);
+    }
 
-  if (render) {
-    return render(props);
-  }
+    if (render) {
+      return render(props);
+    }
 
-  if (typeof children === 'function') {
-    return children.prototype.render
-      ? createElement(children, props)
-      : children(props);
-  }
+    if (typeof children === 'function') {
+      return children.prototype.render
+        ? createElement(children, props)
+        : children(props);
+    }
 
-  if (children && Children.count(children)) {
-    return cloneElement(children, props);
-  }
-};
+    if (children && Children.count(children)) {
+      return cloneElement(children, props);
+    }
+  };
