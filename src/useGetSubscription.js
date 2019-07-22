5b5acea1b607a560230d0bade7b26bcb95024371
Arno Saine <arno@mowhi.com>
2019-07-22 17:30:03 +0300

    Remove unnecessary useMemo dependency, function never changes

--- src/useGetSubscription.js
+++ src/useGetSubscription.js
@@ -5,7 +5,6 @@ import getSubscription from './getSubscription';
 export default function useGetSubscription(db, options, subscribe) {
   const key = stringify(options);
   return useMemo(() => getSubscription(db, key, subscribe), [
-    getSubscription,
     db,
     key,
     subscribe
