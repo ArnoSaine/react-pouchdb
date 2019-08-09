dd17eb13a6d665a48c9f4bc194c4de8c059fb7be
Arno Saine <arno@mowhi.com>
2019-08-09 19:21:56 +0300

    Fix excessive re-rendering from `bulkDocs`

--- src/createListenHook.js
+++ src/createListenHook.js
@@ -1,7 +1,16 @@
-import useGetSubscription from './useGetSubscription';
+import { useCallback } from 'react';
 import handleError from './handleError';
+import useGetSubscription from './useGetSubscription';
+import debounceSetValue from './debounceSetValue';
 
 export default useListen => (db, options, subscribe) =>
-  useGetSubscription(db, options, subscribe)
+  useGetSubscription(
+    db,
+    options,
+    useCallback(setValue => setValue |> debounceSetValue(db) |> subscribe, [
+      db,
+      subscribe
+    ])
+  )
   |> (subscription => useListen(subscription, db))
   |> handleError;
--- src/createPouchDB.js
+++ src/createPouchDB.js
@@ -3,6 +3,12 @@ import PouchDB from 'pouchdb';
 export default function createPouchDB({
   // Time to wait for suspended component to actually mount and subscribe.
   synchronousAPITemporarySubscriptionCleanupDelay,
+  // Limit excessive re-rendering from bulkDocs:
+  // Time to wait more changes. Final update is made after this, if there were more updates.
+  // Set null to disable debouncing updates.
+  debounceUpdatesWait,
+  // Limit update frequency.
+  debounceUpdatesMaxWait,
   maxListeners,
   ...options
 }) {
@@ -15,6 +21,10 @@ export default function createPouchDB({
     db.setMaxListeners(maxListeners);
   }
   return Object.assign(db, {
-    reactPouchDBOptions: { synchronousAPITemporarySubscriptionCleanupDelay }
+    reactPouchDBOptions: {
+      synchronousAPITemporarySubscriptionCleanupDelay,
+      debounceUpdatesWait,
+      debounceUpdatesMaxWait
+    }
   });
 }
