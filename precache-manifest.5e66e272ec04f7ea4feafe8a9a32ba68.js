8014777b3b30b04367d82d96a7037f79983f9291
Arno Saine <arno@mowhi.com>
2019-08-18 00:36:02 +0300

    Updates

---
+++ precache-manifest.5e66e272ec04f7ea4feafe8a9a32ba68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c210da13d57fdd20d624b1ea6b9f766e",
+    "url": "/react-pouchdb/index.html"
+  },
+  {
+    "revision": "454812f3e087f38b216e",
+    "url": "/react-pouchdb/static/css/2.13480f0e.chunk.css"
+  },
+  {
+    "revision": "f66e764cbda165d7747a",
+    "url": "/react-pouchdb/static/css/main.7706cca1.chunk.css"
+  },
+  {
+    "revision": "454812f3e087f38b216e",
+    "url": "/react-pouchdb/static/js/2.1106d74e.chunk.js"
+  },
+  {
+    "revision": "f66e764cbda165d7747a",
+    "url": "/react-pouchdb/static/js/main.5db402da.chunk.js"
+  },
+  {
+    "revision": "e325ac3de699348045b6",
+    "url": "/react-pouchdb/static/js/runtime~main.3053164c.js"
+  }
+]);
