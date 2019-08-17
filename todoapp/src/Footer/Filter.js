0dfd97b61bd0da58de2266884ba624713633f396
Arno Saine <arno@mowhi.com>
2019-08-18 00:34:43 +0300

    Use named functions

--- todoapp/src/Footer/Filter.js
+++ todoapp/src/Footer/Filter.js
@@ -1,17 +1,19 @@
 import { NavLink } from 'react-router-dom';
 
-export default () => (
-  <ul className="filters">
-    {[
-      { path: '', title: 'All' },
-      { path: 'active', title: 'Active' },
-      { path: 'completed', title: 'Completed' }
-    ].map(({ path, title }) => (
-      <li key={path}>
-        <NavLink activeClassName="selected" exact replace to={`/${path}`}>
-          {title}
-        </NavLink>
-      </li>
-    ))}
-  </ul>
-);
+export default function Filter() {
+  return (
+    <ul className="filters">
+      {[
+        { path: '', title: 'All' },
+        { path: 'active', title: 'Active' },
+        { path: 'completed', title: 'Completed' }
+      ].map(({ path, title }) => (
+        <li key={path}>
+          <NavLink activeClassName="selected" exact replace to={`/${path}`}>
+            {title}
+          </NavLink>
+        </li>
+      ))}
+    </ul>
+  );
+}
