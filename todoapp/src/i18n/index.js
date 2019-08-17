3efda403cde1eac2d3ab5fb5d0bc493fa8985f74
Arno Saine <arno@mowhi.com>
2019-08-17 20:45:13 +0300

    Add missing exports

--- todoapp/src/i18n/index.js
+++ todoapp/src/i18n/index.js
@@ -2,11 +2,14 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+export const availableLanguages = ['en', 'fi'];
+export const [fallbackLng] = availableLanguages;
+
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng,
     debug: process.NODE_ENV === 'development',
     ns: ['translation'],
     defaultNS: 'translation',
