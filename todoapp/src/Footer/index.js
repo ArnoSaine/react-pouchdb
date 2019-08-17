9496979553bf3a8b2a426d8a1a2980637251236d
Arno Saine <arno@mowhi.com>
2019-08-18 00:33:58 +0300

    Fix: show footer only when there are todos

--- todoapp/src/Footer/index.js
+++ todoapp/src/Footer/index.js
@@ -1,11 +1,18 @@
+import { useFind } from 'react-pouchdb/browser';
 import ClearCompleted from './ClearCompleted';
 import Counter from './Counter';
 import Filter from './Filter';
 
-export default () => (
-  <footer className="footer">
-    <Counter />
-    <Filter />
-    <ClearCompleted />
-  </footer>
-);
+export default function Footer() {
+  const docs = useFind({
+    selector: {}
+  });
+  const { length } = docs;
+  return length ? (
+    <footer className="footer">
+      <Counter />
+      <Filter />
+      <ClearCompleted />
+    </footer>
+  ) : null;
+}
