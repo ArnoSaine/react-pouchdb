508f531403bf3e0ea4b09a07256ca0d3c2372ec2
Arno Saine <arno@mowhi.com>
2019-08-19 22:36:55 +0300

    Make top level element to be value or array

--- todoapp/src/shared/Dynamic.js
+++ todoapp/src/shared/Dynamic.js
@@ -16,30 +16,30 @@ export function addComponents(...sources) {
 export default function Dynamic({ id, ...otherProps }) {
   const { element } = useGet(dbName, { id });
   const t = useT();
-  return (function element({ type, props = null, children }) {
-    function escape(value, key) {
-      return Array.isArray(value)
-        ? do {
-            const [option, ...params] = value;
-            Array.isArray(option)
-              ? option.map(escape)
-              : {
-                  element,
-                  prop(name) {
-                    return otherProps[name ?? key];
-                  },
-                  trans(name) {
-                    return t(name ?? key);
-                  }
-                }[option](...params);
-          }
-        : value;
+  return (function escape(value, key) {
+    function element({ type, props = null, children }) {
+      const startsWith = type[0];
+      return createElement(
+        startsWith === startsWith.toUpperCase() ? components[type] : type,
+        props && props::mapValues(escape),
+        ...(children ? Children.map(escape(children, 'children'), x => x) : [])
+      );
     }
-    const startsWith = type[0];
-    return createElement(
-      startsWith === startsWith.toUpperCase() ? components[type] : type,
-      props && props::mapValues(escape),
-      ...(children ? Children.map(escape(children, 'children'), x => x) : [])
-    );
+    return Array.isArray(value)
+      ? do {
+          const [option, ...params] = value;
+          Array.isArray(option)
+            ? option.map(escape)
+            : {
+                element,
+                prop(name) {
+                  return otherProps[name ?? key];
+                },
+                trans(name) {
+                  return t(name ?? key);
+                }
+              }[option](...params);
+        }
+      : value;
   })(element);
 }
