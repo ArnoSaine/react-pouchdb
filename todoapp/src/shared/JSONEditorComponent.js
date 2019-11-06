b3aa4df141f761de023da7f9c9ff46ecf38814d0
Arno Saine <arno@mowhi.com>
2019-11-06 16:02:24 +0200

    Remove unnecessary onChangeJSON wrapper

--- todoapp/src/shared/JSONEditorComponent.js
+++ todoapp/src/shared/JSONEditorComponent.js
@@ -1,29 +1,24 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import JSONEditor from 'jsoneditor';
 import 'jsoneditor/dist/jsoneditor.css';
 
-export default function JSONEditorComponent({
-  json,
-  mode,
-  onChangeJSON: onChange
-}) {
+export default function JSONEditorComponent({ json, mode, onChangeJSON }) {
   const container = useRef();
   const jsoneditor = useRef();
-  const ref = useRef();
+  const onChangeJSONRef = useRef();
   useEffect(() => {
-    ref.current = onChange;
-  }, [onChange]);
-  const onChangeJSON = useCallback((...args) => ref.current(...args), [ref]);
+    onChangeJSONRef.current = onChangeJSON;
+  }, [onChangeJSON]);
   useEffect(() => {
     jsoneditor.current = new JSONEditor(container.current, {
       mode,
-      onChangeJSON,
+      onChangeJSON: (...args) => onChangeJSONRef.current(...args),
       history: false
     });
     return () => {
       jsoneditor.current.destroy();
     };
-  }, [mode, onChangeJSON]);
+  }, [mode]);
   useEffect(() => {
     if (jsoneditor.current.getText() !== JSON.stringify(json)) {
       jsoneditor.current.update(json);
