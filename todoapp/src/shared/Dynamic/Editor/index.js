895b199213c47d68e16687ef6279dd64cc9c4607
Arno Saine <arno@mowhi.com>
2019-11-25 14:22:46 +0200

    Use Link instead of history.replace

--- todoapp/src/shared/Dynamic/Editor/index.js
+++ todoapp/src/shared/Dynamic/Editor/index.js
@@ -1,35 +1,34 @@
 import useT from 'useT';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 export const KEY = 'edit';
 
 export default (function Editor({ enabled, id, children, history, location }) {
   const t = useT();
   const urlSearchParams = new URLSearchParams(location.search);
+  urlSearchParams.set(KEY, id);
   return (
     <>
-      {urlSearchParams.has(KEY) && (
+      {new URLSearchParams(location.search).has(KEY) && (
         <span style={{ position: 'relative' }}>
-          <span
-            role="img"
-            aria-label={t('edit')}
-            style={{
-              top: 0,
-              right: 0,
-              zIndex: 1,
-              position: 'absolute',
-              transform: 'scale(1) translate(50%, -50%)',
-              boxSizing: 'border-box',
-              cursor: 'pointer'
-            }}
-            onClick={() => {
-              urlSearchParams.set(KEY, id);
-              history.replace({ search: urlSearchParams.toString() });
-            }}
-            title={id}
-          >
-            🔧
-          </span>
+          <Link to={{ search: urlSearchParams.toString() }} replace>
+            <span
+              role="img"
+              aria-label={t('edit')}
+              style={{
+                top: 0,
+                right: 0,
+                zIndex: 1,
+                position: 'absolute',
+                transform: 'scale(1) translate(50%, -50%)',
+                boxSizing: 'border-box',
+                cursor: 'pointer'
+              }}
+              title={id}
+            >
+              🔧
+            </span>
+          </Link>
         </span>
       )}
       {children}
